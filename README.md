db346a8b20c30bab8e0a0bdb5e591ec872a75e88
Marce Benitez <106833298+marcebenitez2@users.noreply.github.com>
2023-03-29 23:32:35 -0300

    Thirteenth project

---
+++ README.md
@@ -0,0 +1,13 @@
+## Welcome 👋👋
+
+# Article Preview
+
+*This was a project that caught my attention because of its beauty and because it contained Js material that I had never seen before, which is mainly why I chose it. When it was time to start I could make a mockup in my head of how I had both the css and the functions I was going to use. The development of the site was going quite well until I finished the desktop version and had to develop the responsive version. Here it really got complicated*
+
+### I used
+
+*   DOM Management
+*   Dynamic style change
+*   Real-time listening event
+
+![Demo](./Demo.jpg)
---
+++ app.js
@@ -0,0 +1,71 @@
+const numberCard = document.querySelector(".number");
+const nameCard = document.querySelector(".name");
+const mmCard = document.querySelector(".month");
+const yyCard = document.querySelector(".year");
+const cvcCard = document.querySelector(".cvc");
+
+const inpNumber = document.querySelector(".inp-number");
+const inpName = document.querySelector(".inp-name");
+const inpmm = document.querySelector(".mm");
+const inpyy = document.querySelector(".yy");
+const inpcvc = document.querySelector(".inp-cvc");
+
+const btnConfirm = document.querySelector(".confirm");
+
+const form = document.querySelector(".form");
+
+const error = document.querySelector('.error')
+
+inpNumber.addEventListener("input", () => {
+  number = inpNumber.value;
+  const number4 = number.match(/.{1,4}/g);
+  const numberC = number4.join(" ");
+  numberCard.textContent = numberC;
+});
+
+inpName.addEventListener("input", () => {
+  let names = inpName.value;
+  names = names.toUpperCase();
+  nameCard.textContent = names;
+});
+
+inpmm.addEventListener("input", () => {
+  let mm = inpmm.value;
+  mmCard.textContent = mm;
+});
+
+inpyy.addEventListener("input", () => {
+  let yy = inpyy.value;
+  yyCard.textContent = yy;
+});
+
+inpcvc.addEventListener("input", () => {
+  let cvc = inpcvc.value;
+  cvcCard.textContent = cvc;
+});
+
+btnConfirm.addEventListener("click", (e) => {
+  e.preventDefault(); // evita que se envíe el formulario y se recargue la página
+  let newContent = `
+    <div class="container-confirm">
+      <div class="circle">✓</div>
+      <h1>THANK YOU</h1>
+      <h4>We've added your card details</h4>
+      <button class="exit">CONTINUE</button>
+    </div>
+   `;
+
+  if (number.length === 16) {
+    form.innerHTML = newContent;
+    
+    form.innerHTML = newContent
+    const backBtn = document.querySelector('.exit');
+    backBtn.addEventListener('click', function() {
+  
+    window.location.reload();})
+  } else {
+    error.classList.add('error-active') 
+    console.log(names.length)
+    console.log(number.length)
+  }
+});
